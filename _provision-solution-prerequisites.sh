91d2fb8bf44b5c22b4dd557745f9f268168aaf39
Waldek Mastykarz <waldek@mastykarz.nl>
2018-07-03 20:36:15 +0200

    Fixed bug in setting SPO SP permissions

--- _provision-solution-prerequisites.sh
+++ _provision-solution-prerequisites.sh
@@ -5,19 +5,19 @@ success "DONE"
 echo "Configure */CLIENTSIDEASSETS Public CDN origin..."
 origin=$(o365 spo cdn origin list --output json | jq '.[] | select(. == "*/CLIENTSIDEASSETS")')
 if [ -z "$origin" ]; then
-echo "Origin doesn't exist. Configuring..."
-o365 spo cdn origin add --origin '*/CLIENTSIDEASSETS' --type 'Public'
-success "DONE"
+  echo "Origin doesn't exist. Configuring..."
+  o365 spo cdn origin add --origin '*/CLIENTSIDEASSETS' --type 'Public'
+  success "DONE"
 else
-success "Origin already exists"
+  warning "Origin already exists"
 fi
 
-echo "Configure web API permissions..."
+echo "Configuring web API permissions..."
 echo "Retrieving SharePoint Online Client Extensibility service principal..."
 sp=$(o365 aad sp get --displayName 'SharePoint Online Client Extensibility' --output json | jq -r '.objectId')
 if [ -z "$sp" ]; then
-error "SharePoint Online Client Extensibility service principal not found in Azure AD"
-exit 1
+  error "SharePoint Online Client Extensibility service principal not found in Azure AD"
+  exit 1
 fi
 success "DONE"
 echo "Retrieving Microsoft Graph service principal..."
@@ -28,30 +28,30 @@ graph=$(cat .tmp | jq -r '.objectId')
 # clean up temp file
 rm .tmp
 if [ -z "$graph" ]; then
-error "Microsoft Graph service principal not found in Azure AD"
-exit 1
+  error "Microsoft Graph service principal not found in Azure AD"
+  exit 1
 fi
 success "DONE"
 echo "Retrieving SharePoint Online Client Extensibility service principal permissions for MS Graph..."
-graphPermissions=$(o365 aad oauth2grant list --clientId $sp --output json | jq -r '.[] | select(.resourceId == "'"$graph"'") | {scope: .scope, resourceId: .resourceId')
+graphPermissions=$(o365 aad oauth2grant list --clientId $sp --output json | jq -r '.[] | select(.resourceId == "'"$graph"'") | {scope: .scope, objectId: .objectId'})
 permissions=("Sites.Read.All" "Contacts.Read" "User.Read.All" "Mail.Read" "Calendars.Read" "Group.Read.All")
 if [ -z "$graphPermissions" ]; then
-echo "No permissions for MS Graph configured yet. Setting..."
-o365 aad oauth2grant add --clientId $sp --resourceId $graph --scope "$(echo $permissions)"
-success "DONE"
+  echo "No permissions for MS Graph configured yet. Setting..."
+  o365 aad oauth2grant add --clientId $sp --resourceId $graph --scope "$(echo $permissions)"
+  success "DONE"
 else
-scope=$(echo $graphPermissions | jq -r '.scope')
-echo "Existing permissions found: $scope. Updating..."
-for permission in "${permissions[@]}"; do
+  scope=$(echo $graphPermissions | jq -r '.scope')
+  echo "Existing permissions found: $scope. Updating..."
+  for permission in "${permissions[@]}"; do
     if [[ ! $scope == *"$permission"* ]]; then
-    scope="$scope $permission"
+      scope="$scope $permission"
     fi
-done
+  done
 
-graphPermissionsResourceId=$(echo $graphPermissions | jq -r '.resourceId')
-echo "Updating permissions to $scope..."
-o365 spo oauth2grant set --grantId $resourceId --scope "$scope"
-success "DONE"
+  graphPermissionsObjectId=$(echo $graphPermissions | jq -r '.objectId')
+  echo "Updating permissions to $scope..."
+  o365 aad oauth2grant set --grantId $graphPermissionsObjectId --scope "$scope"
+  success "DONE"
 fi
 
 # TODO
@@ -61,21 +61,21 @@ fi
 currentTheme=$(o365 spo propertybag get --webUrl $portalUrl --key ThemePrimary)
 theme=$(cat theme.json | jq '.themePrimary')
 if [ ! "$currentTheme" = "$theme" ]; then
-echo "Setting theme..."
-o365 spo theme set --name "Contoso Portal" --filePath ./theme.json
-success "DONE"
-echo "Applying theme..."
-o365 spo theme apply --name "Contoso Portal" --webUrl $portalUrl
-success "DONE"
+  echo "Setting theme..."
+  o365 spo theme set --name "Contoso Portal" --filePath ./theme.json
+  success "DONE"
+  echo "Applying theme..."
+  o365 spo theme apply --name "Contoso Portal" --webUrl $portalUrl
+  success "DONE"
 else
-warning "Theme already set"
+  warning "Theme already set"
 fi
 
-if [ -z "$stockAPIKey" ]; then
-echo "Setting stockAPIKey $stockAPIKey..."
-# TODO don't require to specify the app catalog URL
-o365 spo storageentity set --key "PnP-Portal-AlphaVantage-API-Key" --value "$stockAPIKey" --description "API Key for Alpha Advantage REST Stock service"
-success "DONE"
+if [ ! -z "$stockAPIKey" ]; then
+  echo "Setting stockAPIKey $stockAPIKey..."
+  # TODO don't require to specify the app catalog URL
+  o365 spo storageentity set --key "PnP-Portal-AlphaVantage-API-Key" --value "$stockAPIKey" --description "API Key for Alpha Advantage REST Stock service"
+  success "DONE"
 else
-warning "stockAPIKey not specified. Skipping"
+  warning "stockAPIKey not specified. Skipping"
 fi
