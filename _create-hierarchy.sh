ae76a5dfb82f6428fa54e983a321c830b8791777
Waldek Mastykarz <waldek@mastykarz.nl>
2018-07-03 20:35:51 +0200

    Added suppressing errors from get commands

--- _create-hierarchy.sh
+++ _create-hierarchy.sh
@@ -1,9 +1,7 @@
-portalUrl=$tenantUrl/sites/$(echo $prefix)portal
-
 echo "Provisioning portal site at $portalUrl..."
-site=$(o365 spo site get --url $portalUrl --output json)
+site=$(o365 spo site get --url $portalUrl --output json || true)
 if $(isError "$site"); then
-  echo "Creating site..."
+  echo "Site not found. Creating..."
   o365 spo site add --type CommunicationSite --url $portalUrl --title 'PnP SP Starter Kit' --description 'PnP SP Starket Kit Hub'
   success "DONE"
 else
@@ -13,7 +11,7 @@ fi
 hubsiteId=$(o365 spo hubsite list --output json | jq -r '.[] | select(.SiteUrl == "'"$portalUrl"'") | .ID')
 if [ -z "$hubsiteId" ]; then
   echo "Hubsite not found. Registering..."
-  out=$(o365 spo hubsite register --url $portalUrl --output json)
+  out=$(o365 spo hubsite register --url $portalUrl --output json || true)
   if $(isError "$out"); then
     errorMessage "$out"
     exit 1
@@ -26,9 +24,9 @@ fi
 
 siteUrl=$tenantUrl/sites/$(echo $prefix)hr
 echo "Provisioning site at $siteUrl..."
-site=$(o365 spo site get --url $siteUrl --output json)
+site=$(o365 spo site get --url $siteUrl --output json || true)
 if $(isError "$site"); then
-  echo "Creating site..."
+  echo "Site not found. Creating..."
   o365 spo site add --type TeamSite --url $siteUrl --alias $(echo $prefix)hr --title 'Human Resources' --description 'Human Resources'
   success "DONE"
 else
@@ -40,9 +38,9 @@ success "DONE"
 
 siteUrl=$tenantUrl/sites/$(echo $prefix)marketing
 echo "Provisioning site at $siteUrl..."
-site=$(o365 spo site get --url $siteUrl --output json)
+site=$(o365 spo site get --url $siteUrl --output json || true)
 if $(isError "$site"); then
-  echo "Creating site..."
+  echo "Site not found. Creating..."
   o365 spo site add --type TeamSite --url $siteUrl --alias $(echo $prefix)marketing --title 'Marketing' --description 'Marketing'
   success "DONE"
 else
